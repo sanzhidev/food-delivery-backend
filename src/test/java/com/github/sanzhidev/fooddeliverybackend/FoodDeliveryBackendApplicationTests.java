b8d8113bfa360b82ba81a834e658f46eb344b1eb
sanzhar <sanzhar.agabaev@gmail.com>
2026-02-15 17:11:24 +0500

    Initial Spring Boot project setup with PostgreSQL configuration

---
+++ src/test/java/com/github/sanzhidev/fooddeliverybackend/FoodDeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sanzhidev.fooddeliverybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
