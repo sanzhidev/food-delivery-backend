d23f9857f84b2ce3ebbb3608f3783c88320a221a
sanzhidev <sanzhar.agabaev@gmail.com>
2026-02-17 23:17:52 +0500

    Add User entity with role support

--- src/main/java/com/github/sanzhidev/fooddeliverybackend/FoodDeliveryBackendApplication.java
+++ src/main/java/com/github/sanzhidev/fooddeliverybackend/FoodDeliveryBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodDeliveryBackendApplication {
+public class  FoodDeliveryBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FoodDeliveryBackendApplication.class, args);
---
+++ src/main/java/com/github/sanzhidev/fooddeliverybackend/entity/Role.java
@@ -0,0 +1,8 @@
+package com.github.sanzhidev.fooddeliverybackend.entity;
+
+public enum Role {
+
+    CUSTOMER,
+    COURIER,
+    ADMIN
+}
